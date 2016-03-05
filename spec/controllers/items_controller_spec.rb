1b096da7381c922ec1b8d6a03082c36724a442e7
deejayres <erinresso@gmail.com>
2016-03-05 13:27:27 -0800

    show and create to-do items

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -9,7 +9,19 @@ RSpec.describe ItemsController, type: :controller do
 
   describe "POST create" do
     it "increases the number of items by 1" do
-      expect{post :create, user_id: @my_user.id, item: {name: my_item.name}}.to change(Item, :count).by(1)
+      expect{post :create, user_id: @my_user.id, item: {name: "Example"}}.to change(Item, :count).by(1)
+    end
+
+    it "assigns the new item to @item" do
+      post :create, user_id: @my_user.id, item: {name: my_item.name}
+
+      expect(assigns(:item)).to eq Item.last
+    end
+
+    it "redirects to user#show" do
+      post :create, user_id: @my_user.id, item: {name: my_item.name}
+
+      expect(response).to redirect_to root_path
     end
   end
 end
