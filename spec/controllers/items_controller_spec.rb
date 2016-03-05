5937e8bb0613537a12faa39ff32c298ccdc8309f
Brittany Martin <brittany.jill.martin@gmail.com>
2016-03-05 15:37:00 -0500

    Reworked Factories and updated ItemsController test

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  before do
-    @my_user = create(:user)
+
+  before { User.delete_all }
+  before { Item.delete_all }
+
+  before(:each) do
+    @my_user = FactoryGirl.create(:user)
     sign_in @my_user
   end
+
   let(:my_item) { create(:item, user: @my_user)}
 
   describe "POST create" do
     it "increases the number of items by 1" do
-      expect{post :create, user_id: @my_user.id, item: {name: my_item.name}}.to change(Item, :count).by(1)
+      expect{post :create, user_id: @my_user.id, item: {name: "Example Item!"}}.to change(Item, :count).by(1)
     end
   end
 end
